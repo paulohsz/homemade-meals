044186ff3103eb823dc5643e005b2dc13e66dd63
Paulo Henrique Stocco Zancanaro <paulostoc@gmail.com>
2021-03-27 23:29:02 +0000

    Create food API

---
+++ pages/api/foods/[id].js
@@ -0,0 +1,66 @@
+import dbConnect from '../../../src/utils/dbConnect';
+import Food from '../../../src/models/Food';
+// eslint-disable-next-line no-unused-vars
+import Ingredient from '../../../src/models/Ingredient';
+
+// eslint-disable-next-line consistent-return
+export default async function handler(req, res) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  await dbConnect();
+
+  switch (method) {
+    case 'GET': /* Get a model by its ID */
+      try {
+        const food = await Food.findById(id).populate({ path: 'ingredients.ingredient' });
+        if (!food) {
+          return res.status(400).json({ success: false, error: `Not found! ID_FOOD: ${id}` });
+        }
+        res.status(200).json({ success: true, data: food });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case 'PUT': /* Edit a model by its ID */
+      try {
+        const food = await Food.findByIdAndUpdate(id, req.body, {
+          new: true,
+          runValidators: true,
+        });
+
+        if (!food) {
+          return res.status(400).json({ success: false, data: food });
+        }
+        res.status(200).json({ success: true, data: food });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    case 'DELETE': /* Delete a model by its ID */
+      try {
+        const delFood = await Food.deleteOne({ _id: id });
+        if (!delFood) {
+          return res.status(400).json({ success: false });
+        }
+        if (!delFood) {
+          return res.status(400).json({ success: false });
+        }
+        if (delFood.deletedCount === 0) {
+          return res.status(200).json({ success: true, data: {}, error: `Not found! ID_FOOD: ${id}` });
+        }
+        res.status(200).json({ success: true, data: {} });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
---
+++ src/models/Food.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from 'mongoose';
+
+/* FoodSchema will correspond to a collection in your MongoDB database. */
+const FoodSchema = new Schema({
+  name: {
+    /* The name of this food */
+    type: String,
+    required: [true, 'Please provide a name for this food.'],
+    maxlength: [60, 'Name cannot be more than 60 characters'],
+  },
+  ingredients: [{
+    ingredient: {
+      type: Schema.Types.ObjectId,
+      ref: 'Ingredient',
+      required: [true, 'Please provide a name for this food.'],
+    },
+    value: {
+      type: Number,
+      required: [true, 'Please provide a name for this food.'],
+    },
+  }],
+  coccao: {
+    type: Number,
+  },
+  energy: {
+    type: Number,
+  },
+  recipe: {
+    type: String,
+  },
+},
+{
+  timestamps: true,
+});
+
+export default mongoose.models.Food || mongoose.model('Food', FoodSchema);
