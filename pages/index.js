342a9a0d0efac0ed9a44d2bf97157fe278cb8727
Paulo H S Zancanaro <paulo.zancanaro@bynaric.com>
2021-10-13 09:26:03 +0100

    feat: redirect from client side

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,39 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
-import { getSession } from 'next-auth/client';
 import { Box, Container, Typography } from '@mui/material';
 
-export default function Index({ user }) {
+export default function Index() {
+  const [session] = useSession();
   const { push } = useRouter();
 
   useEffect(() => {
-    if (user) {
-      push('/list/ingredients');
+    if (session) {
+      push('/list/ingredients/');
+    } else {
+      push('/auth/sign-in/');
     }
-  }, [user]);
+  }, [session]);
 
   return (
     <Container maxWidth="sm">
       <Box sx={{ mt: 8 }}>
-        <Typography variant="h5" component="h1" color="grey.800" gutterBottom>
-          {`Welcome ${user.name}!`}
-        </Typography>
-        <Typography variant="subtitle1" component="span" color="grey.A700" gutterBottom>
-          Redirecting...
-        </Typography>
+        {session ? (
+          <>
+            <Typography variant="h5" component="h1" color="grey.800" gutterBottom>
+              {`Welcome ${session.user.name}!`}
+            </Typography>
+            <Typography variant="subtitle1" component="span" color="grey.A700" gutterBottom>
+              Redirecting...
+            </Typography>
+          </>
+        ) : (
+          <Typography variant="h5" component="h1" color="grey.800" gutterBottom>
+            Redirecting...
+          </Typography>
+        )}
+
       </Box>
     </Container>
   );
-}
-Index.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-};
-
-export async function getServerSideProps(ctx) {
-  const session = await getSession(ctx);
-  if (!session) {
-    return {
-      redirect: {
-        destination: '/auth/sign-in',
-        permanent: false,
-      },
-    };
-  }
-  return {
-    props: {
-      user: session.user,
-    },
-  };
 }
