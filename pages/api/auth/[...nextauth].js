d6784b37c6d6c502729791d086ea5eeac1987cea
Paulo H S Zancanaro <paulo.zancanaro@bynaric.com>
2021-10-16 21:47:27 +0100

    feat: add new fields on user

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -15,11 +15,29 @@ export default NextAuth({
   session: {
     jwt: true,
   },
+  callbacks: {
+    async jwt(token, user, account, profile, isNewUser) { // eslint-disable-line no-unused-vars
+      // SignIn (credentials)
+      if (user && token && account.id === 'credentials') return { ...token, user };
+      // Persist the OAuth access_token to the token right after signin
+      return token;
+    },
+    async session(session, user, sessionToken) { // eslint-disable-line no-unused-vars
+      if (user.user) return { user: user.user, expires: session.expires };
+      return session;
+    },
+  },
   // Specify Provider
   providers: [
     Providers.Credentials({
+      id: 'credentials',
+      name: 'credentials - MongoDB + Mongooso',
       async authorize(credentials) {
-        if (!credentials.email || !credentials.email.includes('@') || !credentials.password) {
+        if (
+          !credentials.email
+          || !credentials.email.includes('@')
+          || !credentials.password
+        ) {
           throw new Error('Email and password are required');
         }
 
@@ -34,7 +52,10 @@ export default NextAuth({
         }
 
         // Check hased password with DB password
-        const checkPassword = await compare(credentials.password, foundUser.password);
+        const checkPassword = await compare(
+          credentials.password,
+          foundUser.password,
+        );
         // Incorrect password - send response
         if (!checkPassword) {
           throw new Error('Your email or password was incorrect');
@@ -44,7 +65,7 @@ export default NextAuth({
           id: foundUser._id,
           email: foundUser.email,
           name: foundUser.name,
-          image: { observation: foundUser.observation },
+          observation: foundUser.observation,
         };
       },
     }),
