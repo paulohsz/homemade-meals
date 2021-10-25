0e3e41c8ab2c7f33a208ba63e9654e6bda6463d7
Paulo H S Zancanaro <paulo.zancanaro@bynaric.com>
2021-10-25 22:31:37 +0100

    feat: GridHM

--- src/components/commons/GridHM/index.js
+++ src/components/commons/GridHM/index.js
@@ -1,147 +1,131 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import {
-//   Box,
-//   Grid,
-//   Icon,
-//   IconButton,
-//   Tooltip,
-//   makeStyles,
-//   Collapse,
-//   CircularProgress,
-//   useTheme,
-// } from '@mui/material';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Box,
+  Grid,
+  IconButton,
+  Tooltip,
+  Collapse,
+  CircularProgress,
+} from '@mui/material';
+import { alpha } from '@mui/system';
+import { Edit, Delete } from '@mui/icons-material';
 
-// const useStyles = makeStyles((theme) => ({
-//   row: {
-//     transition: theme.transitions.create(['background', 'background-color'], {
-//       duration: theme.transitions.duration.shortest,
-//     }),
-//     '&:hover': {
-//       backgroundColor: transparentize(0.96, theme.palette.primary.main),
-//     },
-//   },
-// }));
+const BaseButton = (type, title, { onClick }) => (
+  <Tooltip title={title} placement="top">
+    <IconButton size="small" aria-label={title} onClick={(e) => onClick(e)}>
+      {type}
+    </IconButton>
+  </Tooltip>
+);
 
-// const BaseButton = (type, title, { onClick }) => (
-//   <Tooltip title={title} placement="top">
-//     <IconButton color="primary" size="small" onClick={(e) => onClick(e)}>
-//       <Icon>{type}</Icon>
-//     </IconButton>
-//   </Tooltip>
-// );
+export const ViewButton = (e) => BaseButton(<Edit />, 'View', e);
+export const DeleteButton = (e) => BaseButton(<Delete />, 'Delete', e);
 
-// export const ViewButton = (e) => BaseButton('visibility', 'View', e);
-// export const DownloadButton = (e) => BaseButton('download', 'Download', e);
+const GridHM = ({
+  data, columnDefs, loadingData, noRows, overlayNoRowsTemplate,
+}) => (
+  <>
+    <Grid
+      sx={{
+        px: 2,
+        pb: 1,
+        alignItems: 'center',
+        fontWeight: 500,
+      }}
+      container
+    >
+      {columnDefs.columns.map((cellHeader) => (
+        <Grid
+          key={`gh_${cellHeader.field}`}
+          item
+          {...cellHeader.propsCell}
+        >
+          {cellHeader.headerName}
+        </Grid>
+      ))}
+    </Grid>
 
-// const GridHM = ({
-//   data, columnDefs, loadingData, noRows, overlayNoRowsTemplate,
-// }) => {
-//   const classes = useStyles();
-//   const { palette } = useTheme();
-//   return (
-//     <>
-//       <Grid container>
-//         <Box
-//           component={Grid}
-//           bgcolor={transparentize(0.92, palette.primary.main)}
-//           borderBottom={`1px solid ${transparentize(0.90, palette.primary.main)}`}
-//           alignItems="center"
-//           fontSize={12}
-//           fontWeight={600}
-//           borderRadius={12}
-//           xs={12}
-//           container
-//           item
-//         >
-//           {columnDefs.columns.map((cellHeader) => (
-//             <Box
-//               key={`grid_header_${cellHeader.field}`}
-//               component={Grid}
-//               py={2}
-//               px={{ xs: 1, sm: 2 }}
-//               item
-//               {...cellHeader.propsCell}
-//             >
-//               {cellHeader.headerName}
-//             </Box>
-//           ))}
-//         </Box>
-//       </Grid>
+    <Collapse in={loadingData}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        p={4}
+      >
+        <CircularProgress size={30} style={{ marginRight: 12 }} />
+        Loading...
+      </Box>
+    </Collapse>
+    <Collapse in={!loadingData}>
+      {noRows && (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          p={4}
+        >
+            {overlayNoRowsTemplate}
+        </Box>
+      )}
+    </Collapse>
+    {data.map((item) => (
+      <Collapse
+        key={`gd_${item._id}`}
+        in={item.delete === undefined && item.filter}
+      >
+        <Grid
+          sx={{
+            px: 2,
+            py: 1,
+            alignItems: 'center',
+            borderRadius: 2,
+            transition: (theme) => theme.transitions.create(['background-color'], {
+              duration: theme.transitions.duration.shortest,
+              easing: theme.transitions.easing.easeInOut,
+            }),
+            '&:hover': {
+              backgroundColor: (theme) => alpha(theme.palette.primary.main, 0.13),
+              transition: (theme) => theme.transitions.create(['background-color'], {
+                duration: theme.transitions.duration.standard,
+                easing: theme.transitions.easing.easeIn,
+              }),
+            },
+          }}
+          container
+        >
+          {columnDefs.columns.map((cellHeader) => (
+            <Box
+              component={Grid}
+              key={`grid_statemant_${item._id}_${cellHeader.field}`}
+              item
+              {...cellHeader.propsCell}
+            >
+              {cellHeader.cellFunction === undefined
+                ? item[cellHeader.field]
+                : cellHeader.cellFunction(item)}
+            </Box>
+          ))}
+        </Grid>
+      </Collapse>
+    ))}
+  </>
+);
+GridHM.defaultProps = {
+  data: [],
+  columnDefs: {},
+  overlayNoRowsTemplate: 'No data is available to show',
+  noRows: false,
+  loadingData: false,
+};
+GridHM.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+  columnDefs: PropTypes.shape({
+    columns: PropTypes.arrayOf(PropTypes.object),
+  }),
+  overlayNoRowsTemplate: PropTypes.string,
+  loadingData: PropTypes.bool,
+  noRows: PropTypes.bool,
+};
 
-//       <Collapse in={loadingData}>
-//         <Box
-//           display="flex"
-//           justifyContent="center"
-//           alignItems="center"
-//           p={4}
-//         >
-//           <CircularProgress size={30} style={{ marginRight: 12 }} />
-//           Loading...
-//         </Box>
-//       </Collapse>
-//       <Collapse in={!loadingData}>
-//         {noRows && (
-//         <Box
-//           display="flex"
-//           justifyContent="center"
-//           alignItems="center"
-//           p={4}
-//         >
-//             {overlayNoRowsTemplate}
-//         </Box>
-//         )}
-//         <Grid container>
-//           {data.map((item) => (
-//             <Box
-//               component={Grid}
-//               alignItems="center"
-//               key={`grid_statemant_${item.id}`}
-//               borderBottom={`1px solid ${transparentize(0.90, palette.primary.main)}`}
-//               borderRadius={12}
-//               className={classes.row}
-//               fontSize={12}
-//               xs={12}
-//               container
-//               item
-//             >
-//               {columnDefs.columns.map((cellHeader) => (
-//                 <Box
-//                   key={`grid_statemant_${item.id}_${cellHeader.field}`}
-//                   component={Grid}
-//                   py={2}
-//                   px={{ xs: 1, sm: 2 }}
-//                   item
-//                   {...cellHeader.propsCell}
-//                 >
-//                   {cellHeader.cellFunction === undefined
-//                     ? item[cellHeader.field]
-//                     : cellHeader.cellFunction(item)}
-//                 </Box>
-//               ))}
-//             </Box>
-//           ))}
-//         </Grid>
-//       </Collapse>
-//     </>
-//   );
-// };
-// GridHM.defaultProps = {
-//   data: [],
-//   columnDefs: {},
-//   overlayNoRowsTemplate: 'No data is available to show',
-//   noRows: false,
-//   defaultColDef: {
-//     columns: [],
-//   },
-//   loadingData: false,
-// };
-// GridHM.propTypes = {
-//   data: PropTypes.array,
-//   columnDefs: PropTypes.object,
-//   overlayNoRowsTemplate: PropTypes.string,
-//   defaultColDef: PropTypes.object,
-//   loadingData: PropTypes.bool,
-// };
-
-// export default GridHM;
+export default GridHM;
